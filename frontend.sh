920e25f63bcdc43d37721fb55f671c7e801efb7c
anupn <anup.natta@gmail.com>
2024-03-23 11:50:07 +0530

    echo command added

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,23 @@
+echo -e "\e[36mInstalling Nginx \e[0m"
 dnf install nginx -y
+
+echo -e "\e[36mRemoving Old Content \e[0m"
+
 rm -rf /usr/share/nginx/html/*
+
+echo -e "\e[36mDownloading Frontend \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+echo -e "\e[36mExtracting downloaded Content \e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+
+echo -e "\e[36mCopying Configs \e[0m"
 cp /configs/nginx.roboshop.config /etc/nginx/default.d/roboshop.conf
 
+
+echo -e "\e[36mEnabling Nginx \e[0m"
 systemctl enable nginx
+
+echo -e "\e[36mStarting Nginx \e[0m"
 systemctl restart nginx
