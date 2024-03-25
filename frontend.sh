697705a3be82bfbbabbc2036d73f587142e1dfa6
anupn <anup.natta@gmail.com>
2024-03-25 22:16:30 +0530

    echo function

--- frontend.sh
+++ frontend.sh
@@ -1,26 +1,31 @@
 code_dir=$(pwd)
 
-echo -e "\e[32mInstalling Nginx \e[0m"
+
+print_head(){
+  echo -e "\e[32m\e[0m"
+}
+
+print_head "Installing Nginx"
 
 dnf install nginx -y
 
-echo -e "\e[32mRemoving Old Content \e[0m"
+print_head "Removing Old Content"
 
 rm -rf /usr/share/nginx/html/*
 
-echo -e "\e[32mDownloading Frontend \e[0m"
+print_head "Downloading Frontend"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
-echo -e "\e[32mExtracting downloaded Content \e[0m"
+print_head "Extracting downloaded Content"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-echo -e "\e[32mCopying Configs \e[0m"
+print_head "Copying Configs"
 cp ${code_dir}/configs/nginx.roboshop.config /etc/nginx/default.d/roboshop.conf
 
 
-echo -e "\e[32mEnabling Nginx \e[0m"
+print_head "Enabling Nginx"
 systemctl enable nginx
 
-echo -e "\e[32mStarting Nginx \e[0m"
+print_head "Starting Nginx"
 systemctl restart nginx
