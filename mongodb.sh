d6c3a260f03ea827254b4e209f95c2ac6b2dc2cd
anupn <anup.natta@gmail.com>
2024-03-27 20:07:49 +0530

    printing heading

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,20 @@
+code_dir=$(pwd)
+
+print_head(){
+  echo -e "32m$1\e[0m"
+}
+
+print_head "Copying MongoDB config files and setting up MongoDB repo"
+
 cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+
+print_head "Installing MongoDB"
 dnf install mongodb-org -y
+
+print_head "Enable MongoDB"
 systemctl enable mongod
+
+print_head "Start MongoDB"
 systemctl start mongod
 
 # Update listen address from 127.0.0.1 to 0.0.0.0
