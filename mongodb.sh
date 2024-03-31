d91847eeba2e38fd834204012a3cc934c1cec402
anupn <anup.natta@gmail.com>
2024-03-31 20:59:20 +0530

    renamed configs8

--- mongodb.sh
+++ mongodb.sh
@@ -2,10 +2,13 @@ source common.sh
 
 print_head "Copying MongoDB config files and setting up MongoDB repo"
 
-cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+cp ${code_dir}configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>{log_file}
 
 print_head "Installing MongoDB"
-dnf install mongodb-org -y
+dnf install mongodb-org -y &>>{log_file}
+
+print_head "Updating Mondgo DB Listner Address"
+sed -i -e 's/127.0.0.1/0.0.0.0'
 
 print_head "Enable MongoDB"
 systemctl enable mongod
