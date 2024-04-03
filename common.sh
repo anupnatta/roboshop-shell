207b317aeadbeaee0e9397832993cfb1c720389c
anupn <anup.natta@gmail.com>
2024-04-03 12:04:28 +0530

    else condition added2

--- common.sh
+++ common.sh
@@ -2,17 +2,13 @@ code_dir=$(pwd)
 log_file=/tmp/roboshop.log
 rm -f {log_file}
 
-
 print_head(){
   echo -e "\e[34m$1\e[0m"
 }
-
 status_check(){
-
   if {$1 -eq 0}; then
     echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILED\e[0m"
   fi
-
 }
