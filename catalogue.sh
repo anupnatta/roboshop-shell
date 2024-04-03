6e5c38787980c16306db09c1cc7382ae83a1804d
anupn <anup.natta@gmail.com>
2024-04-03 13:09:26 +0530

    adding status check3

--- catalogue.sh
+++ catalogue.sh
@@ -57,5 +57,5 @@ dnf install mongodb-org-shell -y &>>{log_file}
 status_check $?
 
 print_head "Loading Schema"
-mongo --host mongodb.devops69.online </app/schema/catalogue.js $>>{log_file}
+mongo --host mongodb.devops69.online </app/schema/catalogue.js
 status_check $?
