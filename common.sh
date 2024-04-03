b27bd5a84c359ca4b3e7d7e4beebafbbb4017fe4
anupn <anup.natta@gmail.com>
2024-04-03 12:20:26 +0530

    adding status check

--- common.sh
+++ common.sh
@@ -5,11 +5,11 @@ rm -f {log_file}
 print_head() {
   echo -e "\e[34m$1\e[0m"
 }
--->status_check() {
-  if [$1 -eq 0]; then
+
+status_check() {
+  if [ $1 -eq 0 ]; then
     echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILED\e[0m"
   fi
 }
--->!
