ac92e65f935d5a8f265ffda6d4032637e527b886
anupn <anup.natta@gmail.com>
2024-04-04 21:43:33 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -2,7 +2,7 @@ source common.sh
 
 mysql_root_password=$1
 if [ -z "${mysql_root_paswword}" ]; then
-  echo -e "\e31mMissing MYSQL Password argument\e[0m"
+  echo -e "\e[31mMissing MYSQL Password argument\e[0m"
   exit 1
 fi
 print_head "Disabling MYSQL"
