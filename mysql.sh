f2bbf74ca4cf2badd9cc55fa0b84195f249a9c2d
anupn <anup.natta@gmail.com>
2024-04-04 22:11:54 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -1,37 +1,34 @@
 source common.sh
-#password = RoboShop@1
 
 mysql_root_password=$1
-if [ -z "${mysql_root_paswword}" ]; then
-  echo -e "\e[31mMissing MYSQL Password argument\e[0m"
+if [ -z "${mysql_root_password}" ]; then
+  echo -e "\e[31mMissing MySQL Root Password argument\e[0m"
   exit 1
 fi
 
-print_head "Disabling MYSQL"
-dnf module disable mysql -y &>>{log_file}
+print_head "Disabling MySQL 8 Version"
+dnf module disable mysql -y  &>>${log_file}
 status_check $?
 
-print_head "Copying MYSQL repo files"
-cp ${code_dir}/configs/mysql.repo /etc/yum.repos.d/mysql.repo &>>{log_file}
+print_head "Copy MySQL repo file"
+cp ${code_dir}/configs/mysql.repo /etc/yum.repos.d/mysql.repo  &>>${log_file}
 status_check $?
 
-print_head "Installing MYSQL Server"
-dnf install mysql-community-server -y &>>{log_file}
+print_head "Installing MySQL Server"
+yum install mysql-community-server -y  &>>${log_file}
 status_check $?
 
-print_head "Enabling MYSQL"
-systemctl enable mysqld &>>{log_file}
+print_head "Enable MYSQL Service"
+systemctl enable mysqld  &>>${log_file}
 status_check $?
 
-print_head "Starting MYSQL"
-systemctl start mysqld &>>{log_file}
+print_head "Start MySQL Service"
+systemctl start mysqld  &>>${log_file}
 status_check $?
 
-print_head "Root Password Setup"
-mysql_secure_installation --set-root-pass ${mysql_root_password}
+print_head "Set Root Password"
+echo show databases | mysql -uroot -p${mysql_root_password} &>>${log_file}
+if [ $? -ne 0 ]; then
+  mysql_secure_installation --set-root-pass ${mysql_root_password}  &>>${log_file}
+fi
 status_check $?
-
-#echo show database | mysql -uroot -p${mysql_root_password} $>>{log_file}
-#if [ $? -ne 0 ]; then
-#  mysql_secure_installation --set-root-pass ${mysql_root_password} $>>{log_file}
-#fi
